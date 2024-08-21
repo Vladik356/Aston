02c3a17d7ebfd8c6612a4302aad00098a6871ede
Vladislav <44534111+Vladik356@users.noreply.github.com>
2024-08-21 19:14:00 +0300

    Create FactorialCalculator.java

---
+++ src/main/java/org/example/FactorialCalculator.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class FactorialCalculator {
+
+    public long factorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Number must be non-negative.");
+        }
+        long result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
